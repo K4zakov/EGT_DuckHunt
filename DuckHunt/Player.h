49d4a16245430c7c04447a2477d809eccf35a53a
K4zakov <156944367+K4zakov@users.noreply.github.com>
2024-02-26 02:18:13 +0200

    p

--- DuckHunt/Player.h
+++ DuckHunt/Player.h
@@ -1,7 +1,29 @@
-#pragma once
-class Player
-{
-public:
-	
-};
-
+//#pragma once
+//#include <SDL.h>
+//#include <SDL_ttf.h>
+//#include <string>
+//
+//class Player {
+//public:
+//    Player(SDL_Renderer* renderer);
+//    ~Player();
+//
+//    void render();
+//    void shoot();
+//    void hit();
+//    void miss();
+//    int getBullets() const;
+//    int getScore() const;
+//    void reset();
+//    void levelUp();
+//    void levelDown();
+//
+//
+//private:
+//    SDL_Renderer* renderer;
+//    TTF_Font* font;
+//    int score;
+//    int bullets;
+//    int level;
+//    void renderText(const std::string& text, int x, int y, SDL_Color color);
+//};
